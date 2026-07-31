e8434f7e70c489ea43c3f9bead0dad658d4e8950
Akshay10-klr <202227828+Akshay10-klr@users.noreply.github.com>
2026-07-31 21:35:31 +0530

    Create Student.java

---
+++ module_02_oops_acces_modifiers/src/firstpack/Student.java
@@ -0,0 +1,14 @@
+package firstpack;
+
+public class Student extends PrivateAcess {
+	void display() {
+		System.out.println(batch);
+		System.out.println(id);
+		System.out.println(name);
+
+		System.out.println(marks);
+		System.out.println(batch);
+	}
+
+	
+}
