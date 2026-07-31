ae651113a439a0fd1dbb8c8b0dab03971e171fca
Akshay10-klr <202227828+Akshay10-klr@users.noreply.github.com>
2026-07-31 21:35:33 +0530

    Create TestPackageClass.java

---
+++ module_02_oops_package/src/test/TestPackageClass.java
@@ -0,0 +1,14 @@
+package test;
+
+import packagetest.PackageClass;
+
+public class TestPackageClass {
+	
+	public static void main(String[] args) {
+
+		PackageClass test=new PackageClass();
+		test.displayText("Akshay Raj");
+
+	}
+
+}
