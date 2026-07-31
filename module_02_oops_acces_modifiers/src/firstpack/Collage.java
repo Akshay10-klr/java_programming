5d3ab562e86c67efa3fa6e0c57d1c6eb027da4d6
Akshay10-klr <202227828+Akshay10-klr@users.noreply.github.com>
2026-07-31 21:35:30 +0530

    Create Collage.java

---
+++ module_02_oops_acces_modifiers/src/firstpack/Collage.java
@@ -0,0 +1,16 @@
+package firstpack;
+
+public class Collage {//same package differnt class
+
+	public static void main(String[] args) {
+		PrivateAcess pvrt=new PrivateAcess();
+		System.out.println(pvrt.batch);
+		System.out.println(pvrt.id);  //private is is not accessible in another class
+		System.out.println(pvrt.name);
+
+		System.out.println(pvrt.marks);
+		System.out.println(pvrt.batch);
+
+	}
+
+}
